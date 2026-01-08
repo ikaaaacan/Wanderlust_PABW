0e557b65953c416a7a65396033a23512d675b9fa
ikaaacan <riskadeabaki0108@example.com>
2026-01-08 14:12:58 +0700

    k

--- app/Models/Wisatawan.php
+++ app/Models/Wisatawan.php
@@ -3,39 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Wisatawan extends Model {
-    use HasFactory;
-
+class Wisatawan extends Model
+{
+    // Beritahu Laravel kalau tabelnya bernama 'wisatawan'
     protected $table = 'wisatawan';
 
-    protected $primaryKey = 'id_wisatawan';
-
     protected $fillable = [
-        'id_user',
-        'tanggal_lahir',
-        'jenis_kelamin',
-        'alamat',
-        'usia',
-        'status_akun',
-        'kota_asal',
-        'preferensi_wisata'
+        'nama',
+        'email',
+        'status',
     ];
-
-    public function user() {
-        return $this->belongsTo(User::class, 'id_user');
-    }
-
-    public function transaksi() {
-        return $this->hasMany(Transaksi::class, 'id_wisatawan');
-    }
-
-    public function penilaian() {
-        return $this->hasMany(Penilaian::class, 'id_wisatawan');
-    }
-
-    public function bookmark() {
-        return $this->hasMany(Bookmark::class, 'id_wisatawan');
-    }
 }
